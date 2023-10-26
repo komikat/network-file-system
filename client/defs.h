0eb5c3cd1bd82eee4435fccf786e4860986e7a5d
shrssnh <shrssnh@gmail.com>
2023-10-26 15:47:11 +0530

    first

---
+++ client/defs.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+#include <stddef.h>
+
+#define CNT_NM 2049
+#define CNT_SS 2050
+#define NM_SS 2051
+
---
+++ client/main.c
@@ -0,0 +1,7 @@
+#include "defs.h"
+
+int main(){
+    printf("Test\n");
+    printf("testtest\b\b\b\n");
+    return 0;
+}
