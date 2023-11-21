785a853b110aba70e3ae20a0ef0c21cbee2e800d
shrssnh <shrssnh@gmail.com>
2023-11-21 19:58:04 +0530

    absolute search

--- naming_server/functions.c
+++ naming_server/functions.c
@@ -3,6 +3,18 @@
 
 // Function to print tree
 
+struct node *absoluteSearch(char **searchStr, int elno, struct node *root) {
+    if (strcmp(searchStr[0], root->name) == 0) {
+        if (elno == 1) return root;
+        struct node *test;
+        for (int i = 0; i < root->no_child; i++) {
+            test =  abasoluteSearch(&searchStr[1], elno - 1, root->children[i]);
+            if (test != NULL) return test;
+        }
+    }
+    else return NULL;
+}
+
 // void searchServer(char **searchstr, struct node *nd, int elno, int id) {
 //     if (strcmp(nd->name, searchstr[0]) == 0) {
 //         if(elno == 1) {
