f40fc6fc8e1a4b7b8801640b228cfe300c0aece5
0x52657775 <92392221+vintage2k@users.noreply.github.com>
2023-11-21 17:06:50 +0530

    Updated client to handle commands

--- client/main.c
+++ client/main.c
@@ -2,6 +2,7 @@
 
 #define BACKLOG 10
 char CLIENT_BUFFER[CLIENT_BUFFER_LENGTH];
+int expect_storage;
 
 // get characters into the client buffer with client buffer length
 // remove trailing \n
@@ -10,28 +11,68 @@ void fgeter() {
     CLIENT_BUFFER[strcspn(CLIENT_BUFFER, "\n")] = 0;
 }
 
-int sender(int sockfd) {
-    if ((send(sockfd, CLIENT_BUFFER, CLIENT_BUFFER_LENGTH, 0)) == -1) {
+void sender(int sockfd,char * buf, int len) {
+    if ((send(sockfd, buf, len, 0)) == -1) {
         fprintf(stderr, "Issues sending req: %d\n", errno);
-        return 0;
+        exit(0);
     };
-    return 1;
 }
 
 int main() {
     int sockfd = initconn("localhost", PORT_NSC);
-    int check = 0;
-
-    while(1) {
-        printf("%s", "==> ");
+    while(1){
+        printf("%s", "Message: ");
         fgeter();
+        if(!strcmp(CLIENT_BUFFER,"exit")){
+            printf("Exiting...");
+            exit(0);
+        }
+        printf("%s\n", CLIENT_BUFFER);
+        char * msg;
+        char * msg_real;
+        char * saveptr;
 
-        if (strcmp(CLIENT_BUFFER, "EXIT") == 0) break;
-
-        if ((check = sender(sockfd))) printf("Command Sent: %s\n", CLIENT_BUFFER);
-
-        if (cehck) recver(sockfd, CLIENT_BUFFER, CLIENT_BUFFER_LENGTH, 0);
+        strcpy(msg,CLIENT_BUFFER);
+        strcpy(msg,saveptr);
+        strcpy(msg,msg_real);
+        char *token = strtok_r(msg," ",&saveptr);
+        if(!strcmp(token,"CREATE") || !strcmp(token,"DELETE") || !strcmp(token,"GETINFO") || !strcmp(token,"COPY")){
+            expect_storage = 0;
+        }
+        else if(!strcmp(token,"READ") || !strcmp(token,"WRITE") ||  !strcmp(token,"LIST")){
+            expect_storage = 1;
+        }else{
+            printf("ERROR: UNRECOGNIZED COMMAND. RE-Enter\n");
+            continue;
+        }
+        sender(sockfd, msg, strlen(msg));
+        recver(sockfd, CLIENT_BUFFER, CLIENT_BUFFER_LENGTH, 0);
+        if(!expect_storage){                                                                                                 // no need to contact storage server
+            printf("%s\n", CLIENT_BUFFER);
+            if(strchr(CLIENT_BUFFER,'\n')){
+                char option[2] = "\0\0";
+                scanf("%s", option);
+                sender(sockfd,option,strlen(option));
+                recver(sockfd, CLIENT_BUFFER, CLIENT_BUFFER_LENGTH, 0);
+                printf("%s\n", CLIENT_BUFFER);
+            }
+        }else{                                                                                                                  //contacting storage server
+            printf("%s\n", CLIENT_BUFFER);
+            if(strchr(CLIENT_BUFFER,'\n')){
+                char option[2] = "\0\0";
+                scanf("%s", option);
+                sender(sockfd,option,strlen(option));
+                recver(sockfd, CLIENT_BUFFER, CLIENT_BUFFER_LENGTH, 0);
+                printf("%s\n", CLIENT_BUFFER);
+            }
+                                                                                                                                        // now to send to storage server
+                                                                                                         //TODO : read needs to send file size as well
+            int storagesock = initconn("CLIENT_BUFFER", PORT_SSC);
+            sender(storagesock, msg_real, strlen(msg_real));
+            recver(storagesock, CLIENT_BUFFER, CLIENT_BUFFER_LENGTH, 0);
+            close(storagesock);
+        }
+        printf("OPERATION COMPLETE, ON TO THE NEXT!\n");
     }
-
     close(sockfd);
 }
